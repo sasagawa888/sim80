77b00f29f02c6d057774750b3abea27a6c82e391
sasagawa888 <ken-s@tkcnf.or.jp>
2026-02-14 14:45:01 +0900

    fix gettoken

--- asm80.c
+++ asm80.c
@@ -12,13 +12,14 @@
 #define NUL		'\0'
 FILE *input_stream;
 
-typedef enum toktype {LPAREN,RPAREN,COMMA,INTEGER,HEXNUM,HEXNUM1,SYMBOL,LABEL,OTHER,FILEEND} toktype;
-typedef enum backtrack {GO,BACK} backtrack;
+typedef enum toktype { LPAREN, RPAREN, COMMA, INTEGER, HEXNUM, HEXNUM1,
+	SYMBOL, LABEL, OTHER, FILEEND } toktype;
+typedef enum backtrack { GO, BACK } backtrack;
 
 typedef struct token {
-	char ch;
+    char ch;
     backtrack flag;
-	toktype type;
+    toktype type;
     char buf[BUFSIZE];
 } token;
 
@@ -35,12 +36,12 @@ int inttoken(char buf[])
     int i;
     char c;
 
-	i = 0;			// {1234...}
-	while ((c = buf[i]) != NUL){
-	    if (isdigit(c))
-		i++;
-	    else
-		return (0);
+    i = 0;			// {1234...}
+    while ((c = buf[i]) != NUL) {
+	if (isdigit(c))
+	    i++;
+	else
+	    return (0);
     }
     return (1);
 }
@@ -51,33 +52,35 @@ int hextoken(char buf[])
     int i;
     char c;
 
-    if (buf[0] == '0' && (buf[1] == 'x' || buf[1] == 'X')){
-    i = 0;
-    while ((c = buf[i]) != NUL){
-	if (isxdigit(c))
-	    i++;
-	else
-	    return (0);
-    }}
+    if (buf[0] == '0' && (buf[1] == 'x' || buf[1] == 'X')) {
+	i = 2;
+	while ((c = buf[i]) != NUL) {
+	    if (isxdigit(c))
+		i++;
+	    else
+		return (0);
+	}
 	return (1);
+    }
+    return (0);
 }
 
 int hextoken1(char buf[])
 {
-    int i,len;
+    int i, len;
 
     len = strlen(buf);
-    if(buf[len] == 'H' || buf[len] == 'h'){
-        i = len-1;
-        while(i>=0){
-            if(!isxdigit(buf[i]))
-                return(0);
-        i--;
-        }
-        buf[len] = NUL;
-        return(1);
+    if (buf[len - 1] == 'H' || buf[len - 1] == 'h') {
+	i = len - 2;
+	while (i >= 0) {
+	    if (!isxdigit(buf[i]))
+		return (0);
+	    i--;
+	}
+	buf[len - 1] = NUL;
+	return (1);
     }
-    return(0);
+    return (0);
 }
 
 int issymch(char c)
@@ -118,16 +121,21 @@ int symboltoken(char buf[])
 
 int labeltoken(char buf[])
 {
+    int len = strlen(buf);
+    if (len <= 1)
+	return 0;
 
-    if(!symboltoken(buf))
-	    return (0);
-    else{
-        int len = strlen(buf);
-        if(buf[len] == ':') 
-        return(1);
-    }
+    if (buf[len - 1] != ':')
+	return 0;
 
-    return (0);
+    buf[len - 1] = NUL;
+    if (!symboltoken(buf))
+	return 0;
+
+    // if (strlen(buf) > 8) error(...);
+
+    return 1;
+
 }
 
 
@@ -153,16 +161,25 @@ void gettoken(void)
 	return;
     }
 
-    skip:
+    if (stok.ch == ',') {
+	stok.type = COMMA;
+	stok.ch = NUL;
+	return;
+    }
+
+  skip:
     c = fgetc(input_stream);
     while ((c == SPACE) || (c == EOL) || (c == TAB))
 	c = fgetc(input_stream);
 
-    if(c == ';'){
-        while (c != EOL){
-	        c = fgetc(input_stream);
-        }
-        goto skip;
+    if (c == ';') {
+	while (c != EOL && c != EOF)
+	    c = fgetc(input_stream);
+	if (c == EOF) {
+	    stok.type = FILEEND;
+	    return;
+	}
+	goto skip;
     }
 
     switch (c) {
@@ -172,6 +189,7 @@ void gettoken(void)
     case ')':
 	stok.type = RPAREN;
 	break;
+    case ',':
 	stok.type = COMMA;
 	break;
     case EOF:
@@ -180,9 +198,9 @@ void gettoken(void)
     default:{
 	    pos = 0;
 	    stok.buf[pos++] = c;
-	    while (((c = fgetc(input_stream)) != EOL) && (pos < BUFSIZE-1) &&
-		   (c != SPACE) && (c != '(') && (c != ')') &&
-		   (c != '`') && (c != ',') && (c != '@'))
+	    while (((c = fgetc(input_stream)) != EOL)
+		   && (pos < BUFSIZE - 1) && (c != SPACE) && (c != '(')
+		   && (c != ')') && (c != '`') && (c != ','))
 		stok.buf[pos++] = c;
 
 	    stok.buf[pos] = NUL;
@@ -191,21 +209,25 @@ void gettoken(void)
 		stok.type = INTEGER;
 		break;
 	    }
-        if (hextoken(stok.buf)) {
+	    if (hextoken(stok.buf)) {
 		stok.type = HEXNUM;
 		break;
 	    }
-         if (hextoken1(stok.buf)) {
+	    if (hextoken1(stok.buf)) {
 		stok.type = HEXNUM1;
 		break;
 	    }
-        if (labeltoken(stok.buf)) {
-        int len = strlen(stok.buf);
-        stok.buf[len] = NUL;
+	    if (labeltoken(stok.buf)) {
+		int i;
+		for (i = 0; stok.buf[i]; i++)
+		    stok.buf[i] = toupper((unsigned char) stok.buf[i]);
 		stok.type = LABEL;
 		break;
 	    }
 	    if (symboltoken(stok.buf)) {
+		int i;
+		for (i = 0; stok.buf[i]; i++)
+		    stok.buf[i] = toupper((unsigned char) stok.buf[i]);
 		stok.type = SYMBOL;
 		break;
 	    }
