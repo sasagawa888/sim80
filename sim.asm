0591025577f6865f96d0db1acc173c65eeb816cf
sasagawa888 <ken-s@tkcnf.or.jp>
2026-02-17 11:51:53 +0900

    add JR NZ sim80

--- sim.asm
+++ sim.asm
@@ -1,8 +1,15 @@
 ;test sim80
         LD   A,0xFF
+        JP LOOP1
 LOOP:
         DEC  A
         RST 0x28
         JP   NZ,LOOP
+        HALT
+
+LOOP1:
+        DEC  A
+        RST 0x28
+        JR   NZ,LOOP1
         HALT
 ;end
